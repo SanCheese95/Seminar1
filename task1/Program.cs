c415b3f300aca30c8ee15f7079eb276cf3e81e27
Sancheese95 <sasha_pavlyukovec@mail.ru>
2023-07-16 04:01:51 +0300

    done homework

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿///Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число A: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2) Console.WriteLine($"max -> {num1}");
+else Console.WriteLine($"max -> {num2}");
---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число A: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число C: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+if (num2 > num1) max = num2;
+    if (num3 > num2) max = num3;
+Console.Write($"{num1}, {num2}. {num3} -> {max}");
